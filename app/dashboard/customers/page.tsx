17e7f9298f491b9f0f33d9355863e191ff1d7ee5
Yurii <georgi.paliy@gmail.com>
2024-10-16 15:48:06 +0200

    customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,22 @@
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  return (
+    <div className="w-full">
+      <CustomersTable query={query} />
+    </div>
+  )
 }
